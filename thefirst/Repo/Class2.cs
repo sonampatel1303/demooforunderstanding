d5c7b2931c2c45d156b9d2029bdf3b06eac674cb
Sonam <sonampatel1303@gmail.com>
2024-10-06 12:12:40 +0530

    Add project files.

---
+++ thefirst/Repo/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thefirst.Repo
+{
+    internal class Class2
+    {
+        public Class2() { }
+        public void Second(int x) { 
+        Console.WriteLine(x);
+        
+        }
+
+    }
+
+}
---
+++ thefirst/model/Classw.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thefirst.model
+{
+    public class Classw
+    {
+        public string Name {  get; set; }
+        public Classw() { 
+        
+        }
+        public Classw(string name)
+        {
+            Name = name;
+            Console.WriteLine("FIrst");
+        }
+    }
+}
